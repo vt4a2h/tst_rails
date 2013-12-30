c398d4e091c3d8a6af770bc8c02350e9a9a9ef15
vt4a2h <vt4a2h@gmail.com>
2013-12-30 22:55:23 +0700

    added root for heroku _1

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Tst34::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  #root 'application'
+  root 'tst#root'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
